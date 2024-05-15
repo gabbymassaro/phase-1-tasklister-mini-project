473e64e2455105ea311f030bb006acda7343fa50
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-15 08:45:50 -0400

    preventDefault is complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+// document.addEventListener("DOMContentLoaded", () => {
+
+// });
+
+document.addEventListener('click', (e) => {
+  const submitButton = document.querySelectorAll("form")[0];
+  const secondChild = submitButton[1];
+  e.preventDefault(secondChild); 
 });
