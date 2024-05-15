76562cbb5177b33bb6a4469c416f2df175daaec3
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-15 10:22:25 -0400

    add text input to unordered list

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-// document.addEventListener("DOMContentLoaded", () => {
+const submitButton = document.querySelectorAll("form")[0];
+const firstChild = submitButton[0];
+const secondChild = submitButton[1];
+console.log(secondChild)
 
-// });
-
-document.addEventListener('click', (e) => {
-  const submitButton = document.querySelectorAll("form")[0];
-  const secondChild = submitButton[1];
-  e.preventDefault(secondChild); 
+document.addEventListener('DOMContentLoaded', () => {
+  submitButton.addEventListener('submit', (e) => {
+    e.preventDefault(secondChild);
+    listTasks(e.target["new-task-description"].value)
+  }) 
 });
+
+function listTasks(tasks) {
+  let li = document.createElement('li')
+  li.textContent = tasks
+  document.querySelector('#tasks').appendChild(li); 
+}
